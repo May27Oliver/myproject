7f3371ced24678c719e7de0469244abdd66b52f0
Oliver Chen <oliver310338@hotmail.com>
2019-02-11 23:15:36 +0800

    Java and Kotlin variable,data type

---
+++ src/main/java/com/tom/Hello.java
@@ -0,0 +1,18 @@
+package com.tom;
+
+public class Hello {
+    public static void main(String[] args){
+//        System.out.println("Hello World!!");
+        Person p = new Person();
+        p.hello();
+        int age;
+        age = 22;
+        char c = '中';
+        Character c2 = 'A';
+        byte b = 30;
+        float weight = 65.5f;
+        boolean enroll = false;
+        String s = new String("ABCD");
+        System.out.println(enroll);
+    }
+}
